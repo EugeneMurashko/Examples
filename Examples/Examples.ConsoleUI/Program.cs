1b2193741f25ec91aae5754a67e32c1bd7881f2f
eugene.murashka <eugen.murashko@gmail.com>
2021-06-20 12:22:12 +0300

    Init ConsoleUI project

---
+++ Examples/Examples.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examples.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
